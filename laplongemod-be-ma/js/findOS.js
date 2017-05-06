11d4db7de2003ed4a797e3b10887db600ef4d65c
laplongejunior <thierry.ladeuze@yahoo.fr>
2017-05-07 01:22:38 +0200

    Update findOS.js

--- laplongemod-be-ma/js/findOS.js
+++ laplongemod-be-ma/js/findOS.js
@@ -38,7 +38,7 @@ function adaptButtonToOS(buttonID, targetOS, arrayID)
     if (index === -1) m_goodOSfound[m_goodOSfound.length-1] = arrayID;
     newClass = m_matchClass;
   }  
-  else if (targetOS !== window.laplonge_enumOS.UNKNOWN) newClass = m_invalidClass;
+  else if (targetOS !== window.laplonge_enumOS.UNKNOWN) { newClass = m_invalidClass; console.log(newClass); }
   else if (m_goodOSfound.indexOf(arrayID) !== -1) newClass = m_matchClass;
   else newClass = m_unknownClass;
   
