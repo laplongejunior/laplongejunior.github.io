ec81c322e7e765647752061b249e08e43ff15ef0
laplongejunior <thierry.ladeuze@yahoo.fr>
2017-05-07 00:48:15 +0200

    Update md5check.js

--- laplongemod-be-ma/js/md5check.js
+++ laplongemod-be-ma/js/md5check.js
@@ -1,36 +1,34 @@
-var boxDoc;
-var divDoc;
+var m_boxDoc, m_divDoc;
 function initMD5(boxId, divId)
 {
-	boxDoc = document.getElementById(boxId);
-	divDoc = document.getElementById(divId);
+	m_boxDoc = document.getElementById(boxId);
+	m_divDoc = document.getElementById(divId);
 }
 
-var goodMD5;
+var m_goodMD5;
 function setMD5(spanId, MD5hash)
 {
-	goodMD5 = MD5hash;
-	alert(spanId);
-	document.getElementById(spanId).innerHTML = goodMD5;
+	m_goodMD5 = MD5hash;
+	document.getElementById(spanId).innerHTML = m_goodMD5;
 }
 
 function updateMD5()
 {
-	var MD5input = boxDoc.value.trim();
+	var MD5input = m_boxDoc.value.trim();
 	if (MD5input.length == 0)
 	{
-		divDoc.innerHTML = "";
+		m_divDoc.innerHTML = "";
 		return;
 	}
 	
-	var compare = compareHexa(MD5input, goodMD5);
+	var compare = compareHexa(MD5input, m_goodMD5);
 	
 	if (compare === null)
-		divDoc.innerHTML = "<font color='red'>Un MD5 contient 32 caractères</font>";
+		m_divDoc.innerHTML = "<font color='red'>Un MD5 contient 32 caractères</font>";
 	else if (!compare)
-		divDoc.innerHTML = "<font color='red'>Ce MD5 n'est pas le bon : retéléchargez votre fichier</font>";
+		m_divDoc.innerHTML = "<font color='red'>Ce MD5 n'est pas le bon : retéléchargez votre fichier</font>";
 	else
-		divDoc.innerHTML = "<font color='green'>Les MD5 correspondent : votre fichier est à jour</font>";
+		m_divDoc.innerHTML = "<font color='green'>Les MD5 correspondent : votre fichier est à jour</font>";
 }
 
 function compareHexa(badMD5, goodMD5)
