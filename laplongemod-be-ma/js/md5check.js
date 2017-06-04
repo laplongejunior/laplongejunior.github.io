6af1c81c95ee0ec56b835adde806cfc58c95f57a
laplongejunior <thierry.ladeuze@yahoo.fr>
2017-06-04 18:14:52 +0200

    Ajout de la suppression des espaces

--- laplongemod-be-ma/js/md5check.js
+++ laplongemod-be-ma/js/md5check.js
@@ -5,7 +5,7 @@ function initMD5(boxId, divId, keyId)
 {
 	m_boxDoc = document.getElementById(boxId);
 	m_divDoc = document.getElementById(divId);
-	m_goodMD5 = document.getElementById(keyId).innerHTML.replace(/\t|\r?\n|\r/gm,'');
+	m_goodMD5 = document.getElementById(keyId).innerHTML.replace(/\t| |\r?\n|\r/gm,'');
 	console.log(m_goodMD5);
 }
 
