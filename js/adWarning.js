df2dfb7fa46280427f290f81b5096a408decdf23
laplongejunior <thierry.ladeuze@yahoo.fr>
2018-04-24 22:29:56 +0200

    Fixed boolean statement

--- js/adWarning.js
+++ js/adWarning.js
@@ -1,7 +1,6 @@
 function controlAdWarning(warningId)
 {
   var doc = document.getElementById(warningId);
-  console.log(laplongeUtils_isInIFRAME());
-  if (doc !== null && laplongeUtils_isInIFRAME())
+  if (doc !== null && !laplongeUtils_isInIFRAME())
     laplongeUtils_removeElement(doc);
 }
