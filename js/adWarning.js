ec0390593c25eb50efb3ed90b29311094676cb02
laplongejunior <thierry.ladeuze@yahoo.fr>
2018-04-24 22:13:48 +0200

    Transfered method to laplongeUtils

--- js/adWarning.js
+++ js/adWarning.js
@@ -1,12 +1,6 @@
 function controlAdWarning(warningId)
 {
   var doc = document.getElementById(warningId);
-  if (doc !== null)
-    doc.style.visibility = isInIFRAME() ? 'visible':'hidden';
-}
-
-function isInIFRAME()
-{
-    try { return window.self !== window.top; }
-    catch (e) { return true; }
+  if (doc !== null && laplongeUtils_isInIFRAME())
+    laplongeUtils_removeElement(doc);
 }
