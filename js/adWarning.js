1e565a19d2075d6fbcdd710061f0d71c8f52347e
laplongejunior <thierry.ladeuze@yahoo.fr>
2018-04-24 22:07:42 +0200

    Added toggling code

--- js/adWarning.js
+++ js/adWarning.js
@@ -1,1 +1,12 @@
+function controlAdWarning(warningId)
+{
+  var doc = document.getElementById(warningId);
+  if (doc !== null)
+    doc.style.visibility = isInIFRAME() ? 'visible':'hidden';
+}
 
+function isInIFRAME()
+{
+    try { return window.self !== window.top; }
+    catch (e) { return true; }
+}
