ee326749e31c042a4c5d5fe25f7a536648f85e87
laplongejunior <thierry.ladeuze@yahoo.fr>
2018-04-24 22:31:40 +0200

    Update adWarning.js

--- js/adWarning.js
+++ js/adWarning.js
@@ -1,6 +1,7 @@
 function controlAdWarning(warningId)
 {
   var doc = document.getElementById(warningId);
+  console.log(laplongeUtils_isInIFRAME());
   if (doc !== null && !laplongeUtils_isInIFRAME())
     laplongeUtils_removeElement(doc);
 }
