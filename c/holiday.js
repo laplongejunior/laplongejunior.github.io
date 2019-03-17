2d2035ff25bef59886fb5d0b3fe602c5ac80cdb2
laplongejunior <thierry.ladeuze@yahoo.fr>
2019-03-17 11:23:55 +0100

    Update holiday.js

--- c/holiday.js
+++ c/holiday.js
@@ -2,22 +2,23 @@
 X = function() {
 	var today = new Date();
 	var dayOfWeek = today.getDay();
-	var month = today.getMonth();
-	/*
 	var dayOfMonth = today.getDate();
+	/*
+	var month = today.getMonth();
 	var year = today.getFullYear();
 	*/
 	
-	// Ceria le lundi
-	if (dayOfWeek == 1)
-		return false;
+	var lastDay = (dayOfMonth < 20) ? 2 : 1;
 	
 	// Ceria jusqu'au mardi soir
-	if (dayOfWeek == 2) {
+	if (dayOfWeek == lastDay) {
 		var hours = today.getHours();
 		return (hours > 16);
 	}
+	// Ceria le lundi
+	else if (dayOfWeek == 1)
+		return false;
 	
-	// Congé le reste de la semaine
+	// CongÃ© le reste de la semaine
 	return true;
 }
