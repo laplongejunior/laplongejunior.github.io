514d2897f9b0df31b7771d4149e63d3475a1f4d3
laplongejunior <thierry.ladeuze@yahoo.fr>
2021-06-01 08:39:30 +0200

    Added classes

--- topwar/arfr/heures/dev/script.js
+++ topwar/arfr/heures/dev/script.js
@@ -1,4 +1,20 @@
 (function(global){
+  class Diff {
+    constructor(h, m, s) {
+      this.h = h;
+      this.m = m;
+      this.s = s;
+    }
+    constructor(timestamp) {
+      let arr = timestamp.split(':');
+      super(parseInt(arr[0]), parseInt(arr[1]), parseInt(arr[2]));
+    }
+    
+    getTime() {
+      let time = new Date();
+      return new Date(time.getTime()+ ((((h*60)+m*60)+s)*1000) );
+    }
+  }
   class Ruin {
     constructor(id, spoil, owner) {
       this.id = id;
@@ -7,8 +23,7 @@
     }
     
     setCountdown(countdown) {
-      alert("Unfinished setCountdown");
-      return this;
+      this.spoil = countdown.getTime();
     }
   };
 })(this);
