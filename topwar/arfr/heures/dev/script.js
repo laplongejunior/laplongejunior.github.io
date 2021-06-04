4ffa77709269e3a457ad4f1d1b3ee0d52cba8f6b
laplongejunior <thierry.ladeuze@yahoo.fr>
2021-06-04 17:50:33 +0200

    Update script.js

--- topwar/arfr/heures/dev/script.js
+++ topwar/arfr/heures/dev/script.js
@@ -161,7 +161,7 @@
     }
 
     const BASE = 2, ADJUST = 1;
-    const SIDE = 14;
+    const SIDE = 15;
     let gen = new SafeMatrix(SIDE);
     const MIDDLE = SIDE/2;
     const inMiddle = function(index) {
