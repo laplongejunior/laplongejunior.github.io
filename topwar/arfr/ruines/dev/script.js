7abfa575d0406a7effe68c55c68fdec57f67a334
laplongejunior <thierry.ladeuze@yahoo.fr>
2021-06-07 02:19:52 +0200

    Update script.js

--- topwar/arfr/ruines/dev/script.js
+++ topwar/arfr/ruines/dev/script.js
@@ -271,7 +271,7 @@ this._load = function(loadId,listId,buttonId,outputId,saveId,sortId) {
 
       createUI() {
         let ui = global.document.createElement("span");
-        ui.appendChild(global.document.createElement(this.h+":"+this.m+":"+this.s));
+        ui.appendChild(global.document.createTextNode(this.h+":"+this.m+":"+this.s));
         return ui;
       }
     };
