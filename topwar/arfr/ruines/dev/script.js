13588fd0a833c1ed5cd4cff421c8f65c79ecf79d
laplongejunior <thierry.ladeuze@yahoo.fr>
2021-06-17 15:42:44 +0200

    Update script.js

--- topwar/arfr/ruines/dev/script.js
+++ topwar/arfr/ruines/dev/script.js
@@ -429,11 +429,12 @@ global._load = function(loadInput,loadId,listId,buttonId,outputId,saveId,sortId,
           textField.value = '';
           textField.addEventListener('input', event=>this.setOwner(event.target.value));
           const DEF_STYLE = textField.style.display;
+          textField.style.display = 'none';
           
           input.addEventListener('input', event=>{
             const val = event.target.value;
             this.setOwner(val);
-            textField.style.display = (val === UNKNWOWN) ? 'none' : DEF_STYLE;
+            textField.style.display = (val === UNKNOWN) ? 'none' : DEF_STYLE;
           });
           section.appendChild(input);
           
