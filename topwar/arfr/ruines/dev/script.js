329d5a8f290c56c76e41ba4bdd7038bf46387c62
laplongejunior <thierry.ladeuze@yahoo.fr>
2021-06-17 18:17:05 +0200

    Update script.js

--- topwar/arfr/ruines/dev/script.js
+++ topwar/arfr/ruines/dev/script.js
@@ -409,13 +409,11 @@ global._load = function(loadInput,loadId,listId,buttonId,outputId,saveId,sortId,
         data = spoil.unserialize(data);
         this.setSpoil(spoil);
         
-        let pos = parseInt(data.substring(0,1));
+        let pos = parseInt(data.substring(0,1))-1;
         data = data.substring(1);
         let length = parseInt(data.substring(0,1));
-        let own = data.substring(1,1+length);
         
-        if (pos > 0) own = ALLIS[pos-1][0];
-        this.setOwner(own);
+        this.setOwner((0 <= pos && pos < ALLIS.length) ? ALLIS[pos][0] : data.substring(1,1+length));
         return data.substring(1+length);
       }
       createUI() {
